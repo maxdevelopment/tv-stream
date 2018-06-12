7b2c499626241941151068a077e257a4867fbffe
Maksim Naranovich <maksim.naranovich@oxagile.com>
2018-06-12 13:46:54 +0300

    init router

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/maxdevelopment/tv-stream/config"
+import (
+	"github.com/maxdevelopment/tv-stream/config"
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
+	"log"
+	"io/ioutil"
+)
 
 func main() {
 	config.ReadConfig()
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", GetIndex).Methods("GET")
+	router.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("web/dist/"))))
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         config.Get.IP + ":" + config.Get.Port,
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func GetIndex(writer http.ResponseWriter, request *http.Request) {
+	indexFile, err := ioutil.ReadFile("web/index.html")
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	writer.Write(indexFile)
 }
